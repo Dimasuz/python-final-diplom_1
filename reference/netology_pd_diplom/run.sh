5b7c5f143313fbd4a7e559141d8ac1f40723623b
Dimasuz <dimaskus@gmail.com>
2023-05-01 17:27:56 +0300

    01052023

--- reference/netology_pd_diplom/run.sh
+++ reference/netology_pd_diplom/run.sh
@@ -1,17 +1,15 @@
 #!/bin/sh
 
+python manage.py collectstatic --noinput --clear
+
+python manage.py makemigrations
+
 until python manage.py migrate
 do
     echo "Waiting for db to be ready..."
     sleep 2
 done
 
-python manage.py collectstatic --noinput
+gunicorn netology_pd_diplom.wsgi --bind 0.0.0.0:8000 --workers 4 --threads 4
 
-# python manage.py createsuperuser --noinput
-
-#gunicorn backend.wsgi --bind 0.0.0.0:8000 --workers 4 --threads 4
-
-# for debug
-python manage.py runserver 0.0.0.0:8000
 exec "$@"
